59166b424fc45a3c74109afa58aa5447c8d248f4
Nikita Pushkarski <neskuchaev@gmail.com>
2021-01-05 02:24:48 +0300

    updates maxNumber

--- 10_maxNumber.rb
+++ 10_maxNumber.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
 
-firstNumber = 172.169
-secondNumber = 31.514
+firstNumber = 1
+secondNumber = 155
 
 if firstNumber > secondNumber
     puts "Max number is " + firstNumber.to_s
-else
+elsif firstNumber < secondNumber
     puts "Max number is " + secondNumber.to_s
+else 
+    puts "The numbers are equal"
 end
