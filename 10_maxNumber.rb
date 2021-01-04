92af3c83df9a40c48a5a5757cf1ee271e989370c
Nikita Pushkarski <neskuchaev@gmail.com>
2021-01-05 02:23:10 +0300

    adds the maxNumber.rb

---
+++ 10_maxNumber.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+firstNumber = 172.169
+secondNumber = 31.514
+
+if firstNumber > secondNumber
+    puts "Max number is " + firstNumber.to_s
+else
+    puts "Max number is " + secondNumber.to_s
+end
