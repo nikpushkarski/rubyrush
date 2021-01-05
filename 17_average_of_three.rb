491589c2f4ecb8099ddf78d462c4977b091a314d
Nikita Pushkarski <neskuchaev@gmail.com>
2021-01-05 22:46:39 +0300

    adds the average of three numbers task

---
+++ 17_average_of_three.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+puts "Enter the 1st number"
+firstNumber = gets.to_i
+
+puts "Enter the 2nd number"
+secondNumber = gets.to_i
+
+puts "Enter the 3rd number"
+thirdNumber = gets.to_i
+
+average = (firstNumber + secondNumber + thirdNumber) / 3
+puts "The average is " + average.to_s
