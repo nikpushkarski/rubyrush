d4eda3b0ab4c337d636ca259ab4b3950c5186fe6
Nikita Pushkarski <neskuchaev@gmail.com>
2021-01-11 18:25:26 +0300

    fixes the task 36

--- 36_cut_items_from_array.rb
+++ 36_cut_items_from_array.rb
@@ -4,20 +4,20 @@ def cut_items(arr, number)
   arr[0, number]
 end
 
-def create_array()
+def create_array
   count = 1
   initial_array = []
   while count <= 10
     initial_array.push(rand(50))
     count += 1
   end
-  return initial_array
+  initial_array
 end
 
-initial_array = create_array()
+initial_array = create_array
 
 puts "Here is an initial array: #{initial_array}\n\n"
-puts "How many items do you want to cut from the initial array?"
+puts 'How many items do you want to cut from the initial array?'
 cut_number = gets.to_i
 
-puts "#{cut_items(initial_array, cut_number)}"
+puts cut_items(initial_array, cut_number).to_s
